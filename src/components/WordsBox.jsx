3e298ce023abb32a4e695d9330c56c1a8455752a
Tushar <tushar08032005@gmail.com>
2025-06-29 20:22:28 +0530

    update

--- src/components/WordsBox.jsx
+++ src/components/WordsBox.jsx
@@ -9,23 +9,42 @@ const Wordsbox =() =>{
   const [words,setWords]= useState(generate(300))
   const [currentWord,setCurrentWord]=useState('');
   
-  const generateRandomWord = () =>{
-    const index= Math.floor(Math.random()*words.length);
-    setCurrentWord(words[index]);
-  }
+ 
   const {seconds} = useSettings;
 
   const [currCharInd , setCurrCharInd] =useState(0)
   const [currWordInd , setCurrWordInd] =useState(0)
 
   const [time,setTime]=useState(seconds)
-    
+  const [countDown,setCountDown] =useState(seconds);
+  const [end,setEnd]=useState(false);
+
+  const startTimer = () =>{
+    const interval = setInterval(timer,1000)
+     function timer() {
+        setCountDown((prevCountDown)=>{
+             if(prevCountDown === 1)
+             {
+                setEnd(true);
+                clearInterval(interval)
+                return 0;
+             }
+             return prevCountDown-1;
+        })
+     }
+  }
+
+  
+
+
+
   
    const handleKeyDown =(e) =>{
 
    }
 
     return(
+        <div >
         <div className="words-box">
             <div className="words">
                 {
@@ -42,12 +61,14 @@ const Wordsbox =() =>{
                 )
                 }
             </div>
+         </div>
             <input
               type="text"
+             
               onKeyDown={(e) => handleKeyDown(e)}
             />
+       
         </div>
-
         
     )
 }
